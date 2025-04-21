5cc12fcd2352a7543fc9802c575995c43737659d
OTI-OWOM <ezeogouba@gmail.com>
2025-04-21 15:17:45 +0000

    feat: 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let innerTask = true;
-    let innerTask2 = false;
+    let Task = true;
+    let Task2 = false;
   }
 
   return [task, task2];
